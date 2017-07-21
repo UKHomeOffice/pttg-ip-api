152ebddf2e3901f3f0d98867c287b9676497b8ea
Andy Marke <andy.marke@gmail.com>
2017-07-21 08:51:06 +0100

    added jsonignore and payment amount json name

--- src/main/java/uk/gov/digital/ho/proving/income/domain/hmrc/Employments.java
+++ src/main/java/uk/gov/digital/ho/proving/income/domain/hmrc/Employments.java
@@ -1,10 +1,12 @@
 package uk.gov.digital.ho.proving.income.domain.hmrc;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Employments {
     private final Employer employer;
 }
