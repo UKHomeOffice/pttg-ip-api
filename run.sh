277f37f9e6ab4973964a0c6ea9780945d75c4f17
Andy Marke <andy.marke@gmail.com>
2017-08-23 09:29:21 +0100

    revert experiment with JMX ensuring it is turned on

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 NAME=${NAME:-pttg-ip-api}
 
 JAR=$(find . -name ${NAME}*.jar|head -1)
-java ${JAVA_OPTS} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=9092 -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
+java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
