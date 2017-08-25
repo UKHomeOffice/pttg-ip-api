72792bf736ab5705e50a9090dd5c1a8a4503adfd
Andy Marke <andy.marke@gmail.com>
2017-08-25 08:58:43 +0100

    added filters - revert

--- src/main/java/uk/gov/digital/ho/proving/income/alert/sysdig/Event.java
+++ src/main/java/uk/gov/digital/ho/proving/income/alert/sysdig/Event.java
@@ -1,5 +1,6 @@
 package uk.gov.digital.ho.proving.income.alert.sysdig;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,6 +12,7 @@ public class Event {
     private final String name;
     private final String description;
     private final String severity;
+    @JsonIgnore
     private final String event_filter;
     private final Map<String, Object> tags;
 }
