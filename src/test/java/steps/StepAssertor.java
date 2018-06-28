67cc7554a8c9fa14bacfc7fd5c8584782739028e
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2018-06-28 14:51:28 +0100

    EE-3841 - Tidy up the BDD step assertor

--- src/test/java/steps/StepAssertor.java
+++ src/test/java/steps/StepAssertor.java
@@ -63,56 +63,13 @@ public class StepAssertor {
         }
     }
 
-    private static boolean testCategoryCheck(String json, String responseObject, String key, String expected) {
-        if (responseObject.startsWith("Category ")) {
-            String jsonPath = "$.categoryChecks..[?(@.calculationType == '" + responseObject + "')]." + KEY_MAP.get(key);
-            JSONArray jsonData = read(json, jsonPath);
-            assertTrue(jsonData.size() > 0, String.format("%s %s: Unable to find category check\nJSON: %s", responseObject, key, json));
-            String actual = jsonData.get(0).toString().toLowerCase();
-            assertTrue(expected.equals(actual), String.format("%s %s: expected %s but actual was %s\nJSON: %s", responseObject, key, expected, actual, json));
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean testApplicantEmployees(String json, String responseObject, String key, String expected) {
-        if (key.contains("employer name")) {
-            String nino = key.replace("employer name -", "").trim().toUpperCase();
-            String jsonPath = "$.categoryChecks..[?(@.calculationType == '" + responseObject + "')].individuals..[?(@.nino == '" + nino + "')].employers";
-            JSONArray employersWrapper = read(json, jsonPath);
-
-            List<String> employers = new ArrayList<>();
-            Iterator it = ((JSONArray) employersWrapper.get(0)).iterator();
-            while (it.hasNext()) {
-                employers.add(it.next().toString().toLowerCase());
+    private static boolean testHttpResponse(Response response, String responseObject, String key, String expected) {
+        if (responseObject.toLowerCase().equals("http response")) {
+            if (key.toLowerCase().equals("http status")) {
+                String actual = Integer.valueOf(response.getStatusCode()).toString();
+                assertTrue(expected.equals(actual), String.format("Http Status: expected %s but actual was %s", expected, actual));
+                return true;
             }
-            List<String> requiredEmployers = Arrays.stream(expected.split(",")).map(String::trim).collect(Collectors.toList());
-
-            assertTrue(employers.size() == requiredEmployers.size(),
-                String.format("%s %s: expected %d employers but actual was %d\nJSON: %s", responseObject, key, requiredEmployers.size(), employers.size(), json));
-            assertTrue(employers.containsAll(requiredEmployers),
-                String.format("%s %s: expected employers %s but actual was %s\nJSON: %s", responseObject, key, requiredEmployers, employers, json));
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean testCategoryCheckNinos(String json, String responseObject, String key, String expected) {
-        if (responseObject.startsWith("Category ") && key.contains("nino")) {
-            String jsonPath = "$.categoryChecks..[?(@.calculationType == '" + responseObject + "')].individuals..[?(@.nino == '" + expected + "')]";
-            JSONArray jsonData = read(json, jsonPath);
-            assertTrue(jsonData.size() > 0, String.format("%s %s: Unable to find category check nino %s\nJSON: %s", responseObject, key, expected, json));
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean testNinos(String json, String responseObject, String key, String expected) {
-        if (responseObject.toLowerCase().equals("applicant") || responseObject.toLowerCase().equals("partner")) {
-            String jsonPath = "$.individuals..[?(@.nino == '" + expected + "')].nino";
-            JSONArray jsonData = read(json, jsonPath);
-            assertTrue(jsonData.size() > 0, String.format("%s %s: Unable to find nino %s\nJSON: %s", responseObject, key, expected, json));
-            return true;
         }
         return false;
     }
@@ -127,20 +84,89 @@ public class StepAssertor {
         return false;
     }
 
-    private static boolean testHttpResponse(Response response, String responseObject, String key, String expected) {
-        if (responseObject.toLowerCase().equals("http response")) {
-            if (key.toLowerCase().equals("http status")) {
-                String actual = Integer.valueOf(response.getStatusCode()).toString();
-                assertTrue(expected.equals(actual), String.format("Http Status: expected %s but actual was %s", expected, actual));
-                return true;
-            }
+    private static boolean testNinos(String json, String responseObject, String key, String expected) {
+        if (responseObject.toLowerCase().equals("applicant") || responseObject.toLowerCase().equals("partner")) {
+            String jsonPath = getIndividualNinoJsonPath(expected);
+            JSONArray jsonData = read(json, jsonPath);
+            assertTrue(jsonData.size() > 0, String.format("%s %s: Unable to find nino %s\nJSON: %s", responseObject, key, expected, json));
+            return true;
         }
         return false;
     }
+
+    private static boolean testApplicantEmployees(String json, String responseObject, String key, String expected) {
+        if (key.contains("employer name")) {
+            String nino = key.replace("employer name -", "").trim().toUpperCase();
+            String jsonPath = getApplicantEmployeesJsonPath(responseObject, nino);
+            JSONArray employersWrapper = read(json, jsonPath);
+
+            List<String> employers = getEmployersList(employersWrapper);
+            List<String> requiredEmployers = Arrays.stream(expected.split(",")).map(String::trim).collect(Collectors.toList());
+
+            assertTrue(employers.size() == requiredEmployers.size(),
+                String.format("%s %s: expected %d employers but actual was %d\nJSON: %s", responseObject, key, requiredEmployers.size(), employers.size(), json));
+            assertTrue(employers.containsAll(requiredEmployers),
+                String.format("%s %s: expected employers %s but actual was %s\nJSON: %s", responseObject, key, requiredEmployers, employers, json));
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean testCategoryCheckNinos(String json, String responseObject, String key, String expected) {
+        if (responseObject.startsWith("Category ") && key.contains("nino")) {
+            String jsonPath = getCategoryCheckNinoJsonPath(responseObject, expected);
+            JSONArray jsonData = read(json, jsonPath);
+            assertTrue(jsonData.size() > 0, String.format("%s %s: Unable to find category check nino %s\nJSON: %s", responseObject, key, expected, json));
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean testCategoryCheck(String json, String responseObject, String key, String expected) {
+        if (responseObject.startsWith("Category ")) {
+            String jsonPath = getCategoryCheckPropertyJsonPath(responseObject, key);
+            JSONArray jsonData = read(json, jsonPath);
+            String actual = jsonData.get(0).toString().toLowerCase();
+
+            assertTrue(jsonData.size() > 0, String.format("%s %s: Unable to find category check\nJSON: %s", responseObject, key, json));
+            assertTrue(expected.equals(actual), String.format("%s %s: expected %s but actual was %s\nJSON: %s", responseObject, key, expected, actual, json));
+            return true;
+        }
+        return false;
+    }
+
+
+    private static List<String> getEmployersList(JSONArray employersWrapper) {
+        List<String> employers = new ArrayList<>();
+        Iterator it = ((JSONArray) employersWrapper.get(0)).iterator();
+        while (it.hasNext()) {
+            employers.add(it.next().toString().toLowerCase());
+        }
+        return employers;
+    }
+
+
+    private static String getCategoryCheckPropertyJsonPath(String responseObject, String key) {
+        return "$.categoryChecks..[?(@.calculationType == '" + responseObject + "')]." + KEY_MAP.get(key);
+    }
+
+    private static String getApplicantEmployeesJsonPath(String responseObject, String nino) {
+        return "$.categoryChecks..[?(@.calculationType == '" + responseObject + "')].individuals..[?(@.nino == '" + nino + "')].employers";
+    }
+
+    private static String getIndividualNinoJsonPath(String expected) {
+        return "$.individuals..[?(@.nino == '" + expected + "')].nino";
+    }
+
+    private static String getCategoryCheckNinoJsonPath(String responseObject, String expected) {
+        return "$.categoryChecks..[?(@.calculationType == '" + responseObject + "')].individuals..[?(@.nino == '" + expected + "')]";
+    }
+
 
     private static void validateTestParameters(String responseObject, String key, String expected) {
         if (responseObject.equals("")) {
             throw new IllegalArgumentException("No response object provided for key " + key + " with data " + expected);
         }
     }
+
 }
