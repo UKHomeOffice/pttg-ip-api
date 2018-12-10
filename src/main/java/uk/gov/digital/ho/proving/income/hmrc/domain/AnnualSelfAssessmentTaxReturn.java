0f0276d9f8b712b23c88527695be76b07a8b660d
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2018-12-10 11:22:43 +0000

    EE-11090 Ignore self employment profit in self assessment response

--- src/main/java/uk/gov/digital/ho/proving/income/hmrc/domain/AnnualSelfAssessmentTaxReturn.java
+++ src/main/java/uk/gov/digital/ho/proving/income/hmrc/domain/AnnualSelfAssessmentTaxReturn.java
@@ -1,5 +1,6 @@
 package uk.gov.digital.ho.proving.income.hmrc.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @AllArgsConstructor
 @Accessors(fluent = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AnnualSelfAssessmentTaxReturn {
     @JsonProperty
     private final String taxYear;
---
+++ src/test/java/uk/gov/digital/ho/proving/income/hmrc/domain/AnnualSelfAssessmentTaxReturnTest.java
@@ -0,0 +1,36 @@
+package uk.gov.digital.ho.proving.income.hmrc.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import uk.gov.digital.ho.proving.income.application.ServiceConfiguration;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnnualSelfAssessmentTaxReturnTest {
+
+    private ObjectMapper objectMapper = new ServiceConfiguration("", 0, 0).createObjectMapper();
+
+    @Test
+    public void shouldDeserialize() throws IOException {
+        String json = "{ \"taxYear\": \"2018\", \"summaryIncome\": 50 }";
+
+        AnnualSelfAssessmentTaxReturn taxReturn = objectMapper.readValue(json, AnnualSelfAssessmentTaxReturn.class);
+
+        assertThat(taxReturn.taxYear()).isEqualTo("2018");
+        assertThat(taxReturn.summaryIncome()).isEqualTo(new BigDecimal("50"));
+    }
+
+    @Test
+    public void shouldDeserializeWithSelfEmployment() throws IOException {
+        String json = "{ \"taxYear\": \"2018\", \"summaryIncome\": 50, \"selfEmploymentProfit\": 0 }";
+
+        AnnualSelfAssessmentTaxReturn taxReturn = objectMapper.readValue(json, AnnualSelfAssessmentTaxReturn.class);
+
+        assertThat(taxReturn.taxYear()).isEqualTo("2018");
+        assertThat(taxReturn.summaryIncome()).isEqualTo(new BigDecimal("50"));
+    }
+
+}
