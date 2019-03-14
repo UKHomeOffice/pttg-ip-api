f80c7bd4d1a1b1ecbf16b8077ab79d2d8ce10157
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-03-14 09:36:46 +0000

    EE-16064 Add an AuditResultType comparator

---
+++ src/main/java/uk/gov/digital/ho/proving/income/audit/AuditResultTypeComparator.java
@@ -0,0 +1,19 @@
+package uk.gov.digital.ho.proving.income.audit;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static uk.gov.digital.ho.proving.income.audit.AuditResultType.*;
+
+public class AuditResultTypeComparator implements Comparator<AuditResultType> {
+    private static final List<AuditResultType> naturalOrder
+        = Arrays.asList(ERROR, NOTFOUND, FAIL, PASS);
+
+    @Override
+    public int compare(AuditResultType first, AuditResultType second) {
+        int firstIndex = naturalOrder.indexOf(first);
+        int secondIndex = naturalOrder.indexOf(second);
+        return Integer.compare(firstIndex, secondIndex);
+    }
+}
