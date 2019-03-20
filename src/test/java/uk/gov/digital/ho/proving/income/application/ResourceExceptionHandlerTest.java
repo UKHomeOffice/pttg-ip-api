c5803fba29c64d3485edb79af083158fecf9cb77
Guy Sidebottom <guy.sidebottom@digital.homeoffice.gov.uk>
2019-03-20 08:36:25 +0000

    EE-9178: update exception handler test to use updated method of checking log events. Added additional tests for other exception types.

--- src/test/java/uk/gov/digital/ho/proving/income/application/ResourceExceptionHandlerTest.java
+++ src/test/java/uk/gov/digital/ho/proving/income/application/ResourceExceptionHandlerTest.java
@@ -1,27 +1,36 @@
 package uk.gov.digital.ho.proving.income.application;
 
-import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.LoggingEvent;
 import ch.qos.logback.core.Appender;
 import net.logstash.logback.marker.ObjectAppendingMarker;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import uk.gov.digital.ho.proving.income.api.NinoUtils;
 import uk.gov.digital.ho.proving.income.audit.AuditClient;
 import utils.LogCapturer;
 
 import java.util.List;
 
+import static ch.qos.logback.classic.Level.ERROR;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.fail;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static uk.gov.digital.ho.proving.income.application.LogEvent.INCOME_PROVING_AUDIT_FAILURE;
+import static uk.gov.digital.ho.proving.income.application.LogEvent.INCOME_PROVING_SERVICE_RESPONSE_ERROR;
+import static uk.gov.digital.ho.proving.income.application.LogEvent.INCOME_PROVING_SERVICE_RESPONSE_NOT_FOUND;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ResourceExceptionHandlerTest {
@@ -34,6 +43,13 @@ public class ResourceExceptionHandlerTest {
     private AuditClient auditClient;
     @Mock
     private Appender<ILoggingEvent> mockAppender;
+
+    @Before
+    public void setUp() {
+        Logger rootLogger = (Logger) LoggerFactory.getLogger(ResourceExceptionHandler.class);
+        rootLogger.setLevel(ERROR);
+        rootLogger.addAppender(mockAppender);
+    }
 
     @Test
     public void thatNotFoundExceptionDoesNotLogFullNino() {
@@ -55,22 +71,44 @@ public class ResourceExceptionHandlerTest {
     }
 
     @Test
-    public void shouldLogEventWhenExceptionThrown() {
-        Logger rootLogger = (Logger) LoggerFactory.getLogger(ResourceExceptionHandler.class);
-        rootLogger.setLevel(Level.ERROR);
-        rootLogger.addAppender(mockAppender);
-
+    public void shouldLogAuditDataException() {
         resourceExceptionHandler.handle(new ApplicationExceptions.AuditDataException("some message"));
 
-        verify(mockAppender).doAppend(argThat(argument -> {
-            LoggingEvent loggingEvent = (LoggingEvent) argument;
-
-            return loggingEvent.getFormattedMessage().equals("some message") &&
-                ((ObjectAppendingMarker) loggingEvent.getArgumentArray()[0]).getFieldName().equals("event_id") &&
-                loggingEvent.getArgumentArray()[0].toString().equals("INCOME_PROVING_AUDIT_FAILURE");
-        }));
+        verifyLogMessage("some message", INCOME_PROVING_AUDIT_FAILURE);
     }
 
+    @Test
+    public void shouldLogMissingServletRequestParameterException() {
+        resourceExceptionHandler.handle(new MissingServletRequestParameterException("missing parameter", "parameter type"));
 
+        verifyLogMessage("Required parameter type parameter 'missing parameter' is not present", INCOME_PROVING_SERVICE_RESPONSE_ERROR);
+    }
 
+    @Test
+    public void shouldLogMissingNoHandlerfoundException() {
+        resourceExceptionHandler.handle(new NoHandlerFoundException("POST", "some url", httpHeaders()));
+
+        verifyLogMessage("No handler found for POST some url", INCOME_PROVING_SERVICE_RESPONSE_NOT_FOUND);
+    }
+
+    private void verifyLogMessage(final String message, LogEvent event) {
+        ArgumentCaptor<ILoggingEvent> captor = ArgumentCaptor.forClass(ILoggingEvent.class);
+        verify(mockAppender, atLeastOnce()).doAppend(captor.capture());
+
+        List<ILoggingEvent> loggingEvents = captor.getAllValues();
+        for (ILoggingEvent loggingEvent : loggingEvents) {
+            LoggingEvent logEvent = (LoggingEvent) loggingEvent;
+            if (logEvent.getFormattedMessage().equals(message) && logEvent.getLevel().equals(ERROR) &&
+                loggingEvent.getArgumentArray()[loggingEvent.getArgumentArray().length - 1].equals(new ObjectAppendingMarker("event_id", event))) {
+                return;
+            }
+        }
+        fail(String.format("Failed to find log with message=\"%s\" and level=ERROR", message));
+    }
+
+    private HttpHeaders httpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(CONTENT_TYPE, APPLICATION_JSON_VALUE);
+        return headers;
+    }
 }
