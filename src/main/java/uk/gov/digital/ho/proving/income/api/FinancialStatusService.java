d75079bd39d17b5bfe98cf3e97db0dac5fa4dcca
Owain Roberts <owain.roberts@bjss.com>
2019-06-04 09:05:10 +0100

    EE-19133 Addressing codestyle issue: variables, return types etc. should not be LinkedHashMap but should instead use the interface, Map

--- src/main/java/uk/gov/digital/ho/proving/income/api/FinancialStatusService.java
+++ src/main/java/uk/gov/digital/ho/proving/income/api/FinancialStatusService.java
@@ -13,10 +13,7 @@ import uk.gov.digital.ho.proving.income.validator.IncomeValidationService;
 import uk.gov.digital.ho.proving.income.validator.domain.IncomeValidationRequest;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class FinancialStatusService {
@@ -29,8 +26,8 @@ public class FinancialStatusService {
         this.incomeValidationService = incomeValidationService;
     }
 
-    LinkedHashMap<Individual, IncomeRecord> getIncomeRecords(List<Applicant> applicants, LocalDate startSearchDate, LocalDate applicationRaisedDate) {
-        LinkedHashMap<Individual, IncomeRecord> incomeRecords = new LinkedHashMap<>();
+    Map<Individual, IncomeRecord> getIncomeRecords(List<Applicant> applicants, LocalDate startSearchDate, LocalDate applicationRaisedDate) {
+        Map<Individual, IncomeRecord> incomeRecords = new LinkedHashMap<>();
 
         for (Applicant applicant : applicants) {
 
@@ -45,7 +42,7 @@ public class FinancialStatusService {
         return incomeRecords;
     }
 
-    FinancialStatusCheckResponse calculateResponse(LocalDate applicationRaisedDate, Integer dependants, LinkedHashMap<Individual, IncomeRecord> incomeRecords) {
+    FinancialStatusCheckResponse calculateResponse(LocalDate applicationRaisedDate, Integer dependants, Map<Individual, IncomeRecord> incomeRecords) {
 
         List<Individual> individuals = new LinkedList<>(incomeRecords.keySet());
 
