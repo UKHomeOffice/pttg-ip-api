1ff9ef6a619045dc263e8ec380714033a460c7fa
Owain Roberts <owain.roberts@bjss.com>
2019-08-02 12:41:05 +0100

    EE-21001 More refactoring of PassStatisticsResultsConsolidator to try and make it clear. Added comments and todo to PassRateStatisticsService to plot out direction of this class

--- src/main/java/uk/gov/digital/ho/proving/income/audit/statistics/PassRateStatisticsService.java
+++ src/main/java/uk/gov/digital/ho/proving/income/audit/statistics/PassRateStatisticsService.java
@@ -67,6 +67,7 @@ public class PassRateStatisticsService {
         }
         return new ArrayList<>(bestResultsByNino.values());
 
+        // TODO EE-21001 - probable new routine:
         // Build up a map where each nino is the key and all the query results for that nino are stored in a list as the value
         // Map<String, List<AuditResult>> resultsByNino = new HashMap<>();
         // for (String correlationId : allCorrelationIds) {
@@ -80,7 +81,7 @@ public class PassRateStatisticsService {
         //
         // Consolidate the results into a single list - for each nino, sort results by date, split list if any 10 day gaps,
         // for each split list of results - calculate best earliest result and put into the final results list.
-        //
+        // return PassRateStatisticsConsolidator.consolidateResults(resultByNino.values())
     }
 
     private AuditResult getAuditResultForCorrelationId(String correlationId) {
--- src/main/java/uk/gov/digital/ho/proving/income/audit/statistics/PassStatisticsResultsConsolidator.java
+++ src/main/java/uk/gov/digital/ho/proving/income/audit/statistics/PassStatisticsResultsConsolidator.java
@@ -5,13 +5,11 @@ import org.springframework.stereotype.Component;
 import uk.gov.digital.ho.proving.income.audit.AuditResult;
 import uk.gov.digital.ho.proving.income.audit.AuditResultComparator;
 
-import java.time.LocalDate;
-import java.util.*;
-import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.util.Collections.singletonList;
 
 @Component
 public class PassStatisticsResultsConsolidator {
@@ -36,25 +34,24 @@ public class PassStatisticsResultsConsolidator {
     }
 
     List<List<AuditResult>> separateResultsByCutoff(List<AuditResult> results) {
-        List<AuditResult> sortedByDate = results.stream()
-                                                .sorted(Comparator.comparing(AuditResult::date))
-                                                .collect(Collectors.toList());
+        List<AuditResult> sortedByDate = sortByDate(results);
+        List<List<AuditResult>> groupedByCutoff = new ArrayList<>();
 
-        List<AuditResult> sameRequestResults = new ArrayList<>();
-        List<List<AuditResult>> groupedByCutoff = new ArrayList<>(singletonList(sameRequestResults));
+        List<AuditResult> sameRequestResults = startNewGroup(groupedByCutoff);
 
         for (AuditResult auditResult : sortedByDate) {
-            LocalDate dateOfResult = auditResult.date();
-            LocalDate dateOfLastResult = sameRequestResults.isEmpty() ? LocalDate.MAX : sameRequestResults.get(sameRequestResults.size() - 1).date();
+            long dayOfResult = auditResult.date().toEpochDay();
 
-            if (dateOfResult.toEpochDay() - dateOfLastResult.toEpochDay() >= cutoffDays) {
-                sameRequestResults = new ArrayList<>();
-                groupedByCutoff.add(sameRequestResults);
+            if (!sameRequestResults.isEmpty()) {
+                long dayOfPreviousResult = getLast(sameRequestResults).date().toEpochDay();
+
+                if (afterCutoff(dayOfResult, dayOfPreviousResult)) {
+                    sameRequestResults = startNewGroup(groupedByCutoff);
+                }
             }
             sameRequestResults.add(auditResult);
         }
         return filterEmpty(groupedByCutoff);
-
     }
 
     private AuditResult earliestBestResult(List<AuditResult> auditResults) {
@@ -63,9 +60,29 @@ public class PassStatisticsResultsConsolidator {
                            .orElse(null);
     }
 
+    private List<AuditResult> sortByDate(List<AuditResult> results) {
+        return results.stream()
+                      .sorted(Comparator.comparing(AuditResult::date))
+                      .collect(Collectors.toList());
+    }
+
+    private List<AuditResult> startNewGroup(List<List<AuditResult>> groupedByCutoff) {
+        List<AuditResult> newGroup = new ArrayList<>();
+        groupedByCutoff.add(newGroup);
+        return newGroup;
+    }
+
     private List<List<AuditResult>> filterEmpty(List<List<AuditResult>> groupedByCutoff) {
         return groupedByCutoff.stream()
                               .filter(result -> !result.isEmpty())
                               .collect(Collectors.toList());
+    }
+
+    private AuditResult getLast(List<AuditResult> auditResults) {
+        return auditResults.get(auditResults.size() - 1);
+    }
+
+    private boolean afterCutoff(long dayOfResult, long dayOfPreviousResult) {
+        return dayOfResult - dayOfPreviousResult >= cutoffDays;
     }
 }
