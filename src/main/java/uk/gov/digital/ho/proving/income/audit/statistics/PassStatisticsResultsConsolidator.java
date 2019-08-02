7455029db9afd93816944dbf33a988eab519ce2d
Owain Roberts <owain.roberts@bjss.com>
2019-08-02 12:11:08 +0100

    EE-21001 Refactoring PassStatisticsResultsConsolidator for cleaner code / single responsibility

--- src/main/java/uk/gov/digital/ho/proving/income/audit/statistics/PassStatisticsResultsConsolidator.java
+++ src/main/java/uk/gov/digital/ho/proving/income/audit/statistics/PassStatisticsResultsConsolidator.java
@@ -7,7 +7,11 @@ import uk.gov.digital.ho.proving.income.audit.AuditResultComparator;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Collections.singletonList;
 
 @Component
 public class PassStatisticsResultsConsolidator {
@@ -21,43 +25,47 @@ public class PassStatisticsResultsConsolidator {
     }
 
     List<AuditResult> consolidateResults(List<List<AuditResult>> resultsGroupedByNino) {
-        List<AuditResult> consolidatedResults = new ArrayList<>();
-        for (List<AuditResult> results : resultsGroupedByNino) {
-            List<List<AuditResult>> separateResults = separateResultsByCutoff(results);
-            for (List<AuditResult> groupedResult : separateResults) {
-                AuditResult bestResult = groupedResult.stream()
-                                                      .max(resultComparator)
-                                                      .orElse(null);
-                consolidatedResults.add(bestResult);
-            }
-        }
-        return consolidatedResults;
+        List<List<AuditResult>> separatedByCutoff = resultsGroupedByNino.stream()
+                                                                        .map(this::separateResultsByCutoff)
+                                                                        .flatMap(Collection::stream)
+                                                                        .collect(Collectors.toList());
+
+        return separatedByCutoff.stream()
+                                .map(this::earliestBestResult)
+                                .collect(Collectors.toList());
     }
 
     List<List<AuditResult>> separateResultsByCutoff(List<AuditResult> results) {
-        if (results.isEmpty()) {
-            return Collections.emptyList();
-        }
-
-
         List<AuditResult> sortedByDate = results.stream()
                                                 .sorted(Comparator.comparing(AuditResult::date))
                                                 .collect(Collectors.toList());
 
         List<AuditResult> sameRequestResults = new ArrayList<>();
-        List<List<AuditResult>> groupedByCutoff = new ArrayList<>(Collections.singletonList(sameRequestResults));
+        List<List<AuditResult>> groupedByCutoff = new ArrayList<>(singletonList(sameRequestResults));
+
         for (AuditResult auditResult : sortedByDate) {
             LocalDate dateOfResult = auditResult.date();
             LocalDate dateOfLastResult = sameRequestResults.isEmpty() ? LocalDate.MAX : sameRequestResults.get(sameRequestResults.size() - 1).date();
-            if (dateOfResult.toEpochDay() - dateOfLastResult.toEpochDay() < cutoffDays) {
-                sameRequestResults.add(auditResult);
-            } else {
+
+            if (dateOfResult.toEpochDay() - dateOfLastResult.toEpochDay() >= cutoffDays) {
                 sameRequestResults = new ArrayList<>();
                 groupedByCutoff.add(sameRequestResults);
-                sameRequestResults.add(auditResult);
             }
+            sameRequestResults.add(auditResult);
         }
-        return groupedByCutoff;
+        return filterEmpty(groupedByCutoff);
 
+    }
+
+    private AuditResult earliestBestResult(List<AuditResult> auditResults) {
+        return auditResults.stream()
+                           .max(resultComparator)
+                           .orElse(null);
+    }
+
+    private List<List<AuditResult>> filterEmpty(List<List<AuditResult>> groupedByCutoff) {
+        return groupedByCutoff.stream()
+                              .filter(result -> !result.isEmpty())
+                              .collect(Collectors.toList());
     }
 }
