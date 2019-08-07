e699cf1310b49659b78ef88aac4dceb92567a144
Owain Roberts <owain.roberts@bjss.com>
2019-08-07 11:34:38 +0100

    EE-21001 Removing the separateResultsByCutoff method in PassStatisticsResultsConsolidator that has been moved inot the ResultCutoffSeparator

--- src/main/java/uk/gov/digital/ho/proving/income/audit/statistics/PassStatisticsResultsConsolidator.java
+++ src/main/java/uk/gov/digital/ho/proving/income/audit/statistics/PassStatisticsResultsConsolidator.java
@@ -6,13 +6,9 @@ import uk.gov.digital.ho.proving.income.audit.AuditResult;
 import uk.gov.digital.ho.proving.income.audit.AuditResultComparator;
 import uk.gov.digital.ho.proving.income.audit.ResultCutoffSeparator;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toCollection;
 
 @Component
 public class PassStatisticsResultsConsolidator {
@@ -39,44 +35,9 @@ public class PassStatisticsResultsConsolidator {
                                 .collect(Collectors.toList());
     }
 
-    List<AuditResultsGroupedByNino> separateResultsByCutoff(AuditResultsGroupedByNino results) {
-        // TODO OJR EE-21001 Migrating to ResultCutoffSeparator
-        AuditResultsGroupedByNino sortedByDate = sortByDate(results);
-        List<AuditResultsGroupedByNino> groupedByCutoff = new ArrayList<>();
-
-        AuditResultsGroupedByNino sameRequestResults = startNewGroup(groupedByCutoff);
-
-        for (AuditResult auditResult : sortedByDate) {
-
-            if (sameRequestResults.resultAfterCutoff(cutoffDays, auditResult)) {
-                sameRequestResults = startNewGroup(groupedByCutoff);
-            }
-            sameRequestResults.add(auditResult);
-        }
-        return filterEmpty(groupedByCutoff);
-    }
-
     private AuditResult earliestBestResult(AuditResultsGroupedByNino auditResults) {
         return auditResults.stream()
                            .max(resultComparator)
                            .orElse(null);
-    }
-
-    private AuditResultsGroupedByNino sortByDate(AuditResultsGroupedByNino results) {
-        return results.stream()
-                      .sorted(Comparator.comparing(AuditResult::date))
-                      .collect(toCollection(AuditResultsGroupedByNino::new));
-    }
-
-    private AuditResultsGroupedByNino startNewGroup(List<AuditResultsGroupedByNino> groupedByCutoff) {
-        AuditResultsGroupedByNino newGroup = new AuditResultsGroupedByNino();
-        groupedByCutoff.add(newGroup);
-        return newGroup;
-    }
-
-    private List<AuditResultsGroupedByNino> filterEmpty(List<AuditResultsGroupedByNino> groupedByCutoff) {
-        return groupedByCutoff.stream()
-                              .filter(result -> !result.isEmpty())
-                              .collect(Collectors.toList());
     }
 }
