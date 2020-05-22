14092fd838d1ebd5e346c79b2ea85712211bd3c8
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2020-05-22 15:40:47 +0100

    EE-32216: Pick up the ACP CA cert for the truststore from the kube deployment

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 NAME=${NAME:-pttg-ip-api}
 
 certfiles=$(awk '/-----BEGIN CERTIFICATE-----/{filename="acpca"NR; print filename}; {print >filename}' /certs/acp-root.crt)
@@ -11,10 +12,8 @@ done
 
 keytool -importkeystore -destkeystore /app/truststore.jks -srckeystore /opt/jdk/jre/lib/security/cacerts -srcstorepass changeit -noprompt -storepass changeit &> /dev/null
 
-#java ${JAVA_OPTS} -Djavax.net.ssl.trustStore=/app/truststore.jks \
-#                  -Dcom.sun.management.jmxremote.local.only=false \
-#                  -Djava.security.egd=file:/dev/./urandom -jar "${JAR}" | tee ${LOGFILE} 2>&1
+JAR=$(find . -name ${NAME}*.jar | head -1)
 
-
-JAR=$(find . -name ${NAME}*.jar|head -1)
-java ${JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
+java ${JAVA_OPTS} -Djavax.net.ssl.trustStore=/app/truststore.jks \
+                  -Dcom.sun.management.jmxremote.local.only=false \
+                  -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
